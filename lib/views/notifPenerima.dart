14b0d3ee7644d0b6ccf2427f3f91a49a6718ad30
satrianugraha99 <junksatria@gmail.com>
2022-05-25 08:50:52 +0800

    new_notification

--- lib/views/notifPenerima.dart
+++ lib/views/notifPenerima.dart
@@ -1,6 +1,8 @@
 import 'package:SisKa/_routing/routes.dart';
 import 'package:SisKa/models/api/api_service.dart';
 import 'package:SisKa/models/Notif.dart';
+import 'package:SisKa/notificationservice/local_notification_service.dart';
+import 'package:SisKa/views/demo.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:SisKa/utils/utils.dart';
@@ -26,11 +28,67 @@ class _NotifPenerimaScreenState extends State<NotifPenerimaScreen> {
   bool activeSearch = false;
   String messageTitle = "Empty";
   String notificationAlert = "alert";
+  String deviceTokenToSendPushNotification = "";
 
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
   void initState() {
     super.initState();
+
+    // 1. This method call when app in terminated state and you get a notification
+    // when you click on notification app open from terminated state and you can get notification data in this method
+
+    FirebaseMessaging.instance.getInitialMessage().then(
+      (message) {
+        print("FirebaseMessaging.instance.getInitialMessage");
+        if (message != null) {
+          print("New Notification");
+          if (message.data['_id'] != null) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => Demo(
+                  id: message.data['_id'],
+                ),
+              ),
+            );
+          }
+        }
+      },
+    );
+
+    // 2. This method only call when App in forground it mean app must be opened
+    FirebaseMessaging.onMessage.listen(
+      (message) {
+        print("FirebaseMessaging.onMessage.listen");
+        if (message.notification != null) {
+          print(message.notification!.title);
+          print(message.notification!.body);
+          print("message.data11 ${message.data}");
+          LocalNotificationService.createanddisplaynotification(message);
+
+        }
+      },
+    );
+
+    // 3. This method only call when App in background and not terminated(not closed)
+    FirebaseMessaging.onMessageOpenedApp.listen(
+      (message) {
+        print("FirebaseMessaging.onMessageOpenedApp.listen");
+        if (message.notification != null) {
+          print(message.notification!.title);
+          print(message.notification!.body);
+          print("message.data22 ${message.data['_id']}");
+        }
+      },
+    );
+
+  }
+
+  Future<void> getDeviceTokenToSendNotification() async {
+    final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+    final token = await _fcm.getToken();
+    deviceTokenToSendPushNotification = token.toString();
+    print("Token Value $deviceTokenToSendPushNotification");
   }
 
   @override
@@ -113,6 +171,7 @@ class _NotifPenerimaScreenState extends State<NotifPenerimaScreen> {
   }
 
   Widget build(BuildContext context) {
+    getDeviceTokenToSendNotification();
     onSetupFirebaseMessaging();
     listenForeground();
     return Scaffold(
